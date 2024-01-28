2d932187d83e0c1f95fcb8abf1ac3d3a3d508d7f
username <wecp-dummy@gmail.com>
2024-01-28 16:18:39 +0000

    Implemented Service classes for JDBC and Collections Implemented Service classes using JDBC and Collections only for AccountService, CustomerService, TransactionService

--- backend/src/main/java/com/wecp/progressive/service/AccountServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/AccountServiceImpl.java
@@ -1,7 +1,80 @@
 package com.wecp.progressive.service;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.wecp.progressive.dao.AccountDAO;
+import com.wecp.progressive.entity.Accounts;
+
+public class AccountServiceImpl  implements AccountService {
+
+    private static List<Accounts> accountsList = new ArrayList<Accounts>();
+    private AccountDAO accountDao;
 
 
-public class AccountServiceImpl  {
+    public AccountServiceImpl(AccountDAO accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    @Override
+    public List<Accounts> getAllAccounts() throws SQLException {
+        return accountDao.getAllAccounts();
+    }
+
+    @Override
+    public List<Accounts> getAccountsByUser(int userId) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Accounts getAccountById(int accountId) throws SQLException {
+        return accountDao.getAccountById(accountId);
+    }
+
+    @Override
+    public int addAccount(Accounts accounts) throws SQLException {
+        return accountDao.addAccount(accounts);
+    }
+
+    @Override
+    public void updateAccount(Accounts accounts) throws SQLException {
+        accountDao.updateAccount(accounts);
+    }
+
+    @Override
+    public void deleteAccount(int accountId) throws SQLException {
+        accountDao.deleteAccount(accountId);
+    }
+
+    @Override
+    public List<Accounts> getAllAccountsSortedByBalance() throws SQLException {
+        List<Accounts> accountsList = accountDao.getAllAccounts();
+        Collections.sort(accountsList);
+        return accountsList;
+    }
+
+    @Override
+    public List<Accounts> getAllAccountsFromArrayList() {
+        return accountsList;
+    }
+
+    @Override
+    public List<Accounts> addAccountToArrayList(Accounts accounts) {
+        accountsList.add(accounts);
+        return accountsList;
+    }
+
+    @Override
+    public List<Accounts> getAllAccountsSortedByBalanceFromArrayList() {
+        Collections.sort(accountsList);
+        return accountsList;
+    }
+
+    @Override
+    public void emptyArrayList() {
+        accountsList.clear();
+    }
 
 }
--- backend/src/main/java/com/wecp/progressive/service/CustomerServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/CustomerServiceImpl.java
@@ -1,5 +1,74 @@
 package com.wecp.progressive.service;
 
-public class CustomerServiceImpl {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.wecp.progressive.dao.CustomerDAO;
+import com.wecp.progressive.entity.Customers;
+
+public class CustomerServiceImpl implements CustomerService {
+
+    private static List<Customers> customerList = new ArrayList<Customers>();
+    private CustomerDAO customerDAO;
+
+    public CustomerServiceImpl(CustomerDAO customerDAO){
+        this.customerDAO = customerDAO;
+    }
+
+    @Override
+    public List<Customers> getAllCustomers() throws SQLException {
+        return customerDAO.getAllCustomers();
+    }
+
+    @Override
+    public Customers getCustomerById(int customerId) throws SQLException {
+        return customerDAO.getCustomerById(customerId);
+    }
+
+    @Override
+    public int addCustomer(Customers customers) throws SQLException {
+        return customerDAO.addCustomer(customers);
+    }
+
+    @Override
+    public void updateCustomer(Customers customers) throws SQLException {
+        customerDAO.updateCustomer(customers);
+    }
+
+    @Override
+    public void deleteCustomer(int customerId) throws SQLException {
+        customerDAO.deleteCustomer(customerId);
+    }
+
+    @Override
+    public List<Customers> getAllCustomersSortedByName() throws SQLException {
+        List<Customers> customersList = getAllCustomers();
+        Collections.sort(customersList);
+        return customersList;
+    }
+
+    @Override
+    public List<Customers> getAllCustomersFromArrayList() {
+        return customerList;
+    }
+
+    @Override
+    public List<Customers> addCustomersToArrayList(Customers customers) {
+        customerList.add(customers);
+        return customerList;
+    }
+
+    @Override
+    public List<Customers> getAllCustomersSortedByNameFromArrayList() {
+        Collections.sort(customerList);
+        return customerList;
+    }
+
+    @Override
+    public void emptyArrayList() {
+        customerList.clear();
+    }
 
 }
