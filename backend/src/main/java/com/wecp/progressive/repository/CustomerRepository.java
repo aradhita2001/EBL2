2a6b385a05b617e23230ad4231af771e326073a9
username <wecp-dummy@gmail.com>
2024-02-12 15:06:32 +0000

    Change in customerRepository to pass test case but never used Changes to be committed: 	modified:   backend/src/main/java/com/wecp/progressive/repository/CustomerRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/CustomerRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/CustomerRepository.java
@@ -10,5 +10,7 @@ import com.wecp.progressive.entity.Customers;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customers, Integer> {
+    //Not used as we can use findById(int id)
+    public Customers findByCustomerId(int id);
     public Optional<Customers> findByNameAndEmail(String name, String email);
 }
